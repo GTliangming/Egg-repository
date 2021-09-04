fb0be355169a37fa7ced56f6d84ff177ffc60203
liangming1 <liangming1@lightinthebox.com>
2021-09-04 16:00:18 +0800

    delete cookie and session

--- app/controller/user.ts
+++ app/controller/user.ts
@@ -8,66 +8,6 @@ export default class UserController extends Controller {
     ctx.body = { message: '注册成功！', code: 1 };
   }
 
-  // // 登录
-  // public async Login() {
-  //   const { ctx, app } = this;
-  //   const { username, password } = ctx.request.body;
-  //   // 生成 token 加盐
-  //   const token = app.jwt.sign({
-  //     id: 1,
-  //     username,
-  //     exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60), // token 有效期为 24 小时
-  //     // exp: Math.floor(Date.now() / 1000) + 10, // 测试 --- token 有效期为 10秒
-  //   }, app.config.jwt.secret);
-  //   const newPWd = md5(password);
-  //   const result = decodeMd5(newPWd);
-  //   consola.info(2222, username, password);
-  //   ctx.body = { message: '登录成功2！', code: 1, data: { token, newPWd, result } };
-
-  // }
-
-  // // 获取用户信息
-  // public async GetUserInfo() {
-  //   const ctx = this.ctx;
-  //   ctx.body = { message: '获取成功', code: 1 };
-  // }
-
-  // cookie
-  public async SetAuth() {
-    this.ctx.cookies.set('auth', 'test', {
-      maxAge: 24 * 3600 * 1000, // 过期时间  1天
-      httpOnly: true,
-      signed: true, // 对cookie进行签名，防止被修改
-      encrypt: true, // 对cookie进行加密， 如果加密，则在获取的时候需要对cookie进行解密
-    });
-    this.ctx.body = { message: '设置cookie成功', code: 200 };
-  }
-  public async Auth() {
-    // cookie
-    const cookie = this.ctx.cookies.get('auth', { encrypt: true });
-    this.ctx.body = { message: `刚才设置的cookie是: ${cookie}`, code: 200 };
-  }
-  public async ClearAuth() {
-    // cookie
-    this.ctx.cookies.set('auth', null);
-    this.ctx.body = { message: 'cookie清除成功', code: 200 };
-  }
-
-
-  // session
-  // cookie
-  public async setSession() {
-    this.ctx.session.auth = 'test';
-    this.ctx.body = { message: '设置session成功', code: 200 };
-  }
-  public async getSession() {
-    // cookie
-    consola.log(this.ctx.session);
-    const session = this.ctx.session.auth;
-    this.ctx.body = { message: `刚才设置的session是: ${session}`, code: 200 };
-  }
-
-
   // jwt
   // 登录
   public async Login() {
--- app/router.ts
+++ app/router.ts
@@ -2,7 +2,6 @@ import { Application } from 'egg';
 export default (app: Application) => {
   const { controller, router, middleware } = app;
   const _jwt = middleware.jwt(app.config.jwt.secret);
-  const _Cookie = middleware.cookie('auth');
   // const _Session = middleware.session();
   /* 工具路由 */
 
@@ -20,16 +19,4 @@ export default (app: Application) => {
   router.post('/api/user/login', controller.user.Login);
   // 获取用户信息
   router.post('/api/user/getUserInfo', _jwt, controller.user.GetUserInfo);
-
-
-  // 授权验证
-  // cookie校验
-  router.get('/api/user/setauth', controller.user.SetAuth);
-  router.get('/api/user/auth', _Cookie, controller.user.Auth);
-  router.get('/api/user/clearauth', _Cookie, controller.user.ClearAuth);
-
-
-  // session校验
-  router.get('/api/user/setsession', controller.user.setSession);
-  router.get('/api/user/session', controller.user.getSession);
 };
